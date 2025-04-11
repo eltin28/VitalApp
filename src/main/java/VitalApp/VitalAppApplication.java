98496cff2e035964e69047617d08717bacb779ab
eltin28 <juanj.herrerao@uqvirtual.edu.co>
2025-04-11 09:06:10 -0500

    Inicialización repo

---
+++ src/main/java/VitalApp/VitalAppApplication.java
@@ -0,0 +1,11 @@
+package VitalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VitalAppApplication.class, args);
+    }
+}
